5b037d93dbd9f617e98722733ae34f5ff0e65de8
siddharth <siddharth.singh0701@gmail.com>
2021-10-31 01:45:41 +0530

    Cypress Test Cases

---
+++ cypress/integration/1-getting-started/Test1.js
@@ -0,0 +1,12 @@
+/// <reference types= Cypress />
+
+it('My First Test suite',function()
+{
+describe('My First TestCase',function(){
+
+    cy.visit("https://courses.rahulshettyacademy.com/courses")
+    
+})
+
+})
+
