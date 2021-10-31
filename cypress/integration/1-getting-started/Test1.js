890396c26d0ebc71f2efa9e809cd5a387aba53f2
siddharth <siddharth.singh0701@gmail.com>
2021-10-31 16:25:03 +0530

    assertion ,eq,each and array iteration introduce

--- cypress/integration/1-getting-started/Test1.js
+++ cypress/integration/1-getting-started/Test1.js
@@ -1,10 +1,36 @@
-/// <reference types= Cypress />
+/// <reference types="Cypress" />
 
 it('My First Test suite',function()
 {
 describe('My First TestCase',function(){
 
-    cy.visit("https://courses.rahulshettyacademy.com/courses")
+    cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/")
+    //selenium get  hit the url , Cypress get acts like find element in selenium
+    cy.get('.search-keyword').type('ca')
+    cy.wait(2000)
+    cy.get('.product:visible').should('have.length',4)
+    //Parent child chaining
+    cy.get('.products').find('.product').should('have.length',4)
+    cy.get('.products').find('.product').eq(2).contains("ADD TO CART").click()
+    cy.get('.products').find('.product').each(($el,index,$list) => {
+
+      const vegtext=  $el.find('h4.product-name').text()
+      if(vegtext.includes('Cashews')){
+         cy.wrap($el).find('button').click()
+      }
+    })
+    //assert if logo text is correctly displayed
+cy.get('.brand').should('have.text','GREENKART')
+ 
+//this is to print in logs
+cy.get('.brand').then(function(logoelement)
+{
+    cy.log(logoelement.text())
+ 
+})
+//const logo=cy.get('.brand')
+//cy.log(cy.get('.brand').text())
+// cy.log(logo.text())
     
 })
 
