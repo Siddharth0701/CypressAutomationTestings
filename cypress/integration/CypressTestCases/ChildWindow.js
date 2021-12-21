030ef4b26d24d9037303fbdfca0e6b6440c79f24
Siddharth <siddharth.singh0701@gmail.com>
2021-12-22 00:56:24 +0530

    ChildWindow

---
+++ cypress/integration/CypressTestCases/ChildWindow.js
@@ -0,0 +1,19 @@
+/// <reference types="Cypress" />
+ 
+describe('My Second Test Suite', function() 
+{
+ 
+it('My FirstTest case',function() {
+ 
+//Check boxes
+cy.visit("https://rahulshettyacademy.com/AutomationPractice/")
+cy.get('a#opentab').invoke('removeAttr','target').click()
+ 
+
+
+ 
+    
+})
+ 
+ 
+})
